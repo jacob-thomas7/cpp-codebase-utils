5b0e54a5aeb2f8045ec30183ebb2f84f74bd18ac
Jacob Thomas <jacob.thomas7852@gmail.com>
2025-12-29 17:47:12 -0700

    Project Structure

---
+++ src/utils/metadata.cpp
@@ -0,0 +1,9 @@
+#include "core/systems/metadata.hpp"
+
+namespace core::systems
+{
+    std::ostream& operator<<(std::ostream& os, const Version& version)
+    {
+        return os << (int) version.version_major << '.' << (int) version.version_minor << '.' << (int) version.version_patch;
+    }
+}
